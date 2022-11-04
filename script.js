1b9e9003adeab67e8cc779ada48a7652967b9947
Dillon Richardson <dillonrichardson6@gmail.com>
2022-11-04 17:39:24 -0400

    Completed functional version, may add additional styling later for button color upon click

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
+//declare variables
+let playerWins = 0;
+let playerLosses = 0;
+let currentSelection = '';
+let computerSelection = '';
 const buttons = document.querySelectorAll('button');
+const winText = document.querySelector('#wins');
+const lossText = document.querySelector('#losses');
+const results = document.querySelector('#output');
 
 buttons.forEach(function (button){
   button.addEventListener('click', function (e) {
-    console.log(button.classList)
+    currentSelection = button.id;
+    computerSelection = getComputerChoice();
+    
+    output.textContent = playRound(currentSelection, computerSelection);
+
+    //Will return to this later to add color to the buttons upon click
+    /*if (currentSelection === computerSelection){
+        button.style.background = 'yellow';
+    }else{
+        button.style.background = 'blue';
+        document.getElementById(computerSelection).style.background = 'red';
+    }*/
+
+    winText.textContent = `WINS: ${playerWins}`;
+    lossText.textContent = `LOSSES ${playerLosses}`;
   })
 });
 
+
+//function declarations below
 function getComputerChoice(){
-    result = Math.ceil((Math.random() * 3))
+    let result = Math.ceil((Math.random() * 3))
     
     if (result == 1){
         return "rock";
@@ -22,54 +46,28 @@ function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
 
     if (playerSelection === "rock" && computerSelection === "rock"){
-        return "It was a tie. Play again to determine the winner out of five!";
+        return "It was a tie, no points added";
     }else if (playerSelection === "rock" && computerSelection === "paper"){
+        playerLosses += 1;
         return "You lose! Rock lost to Paper";
     }else if (playerSelection === "rock" && computerSelection === "scissors"){
+        playerWins += 1;
         return "You won! Rock beat Scissors"
     }else if (playerSelection === "paper" && computerSelection === "paper"){
-        return "It was a tie. Play again to determine the winner out of five!";
+        return "It was a tie, no points added";
     }else if (playerSelection === "paper" && computerSelection === "scissors"){
+        playerLosses += 1;
         return "You lose! Paper lost to Scissors";
     }else if (playerSelection === "paper" && computerSelection === "rock"){
+        playerWins += 1;
         return "You won! Paper beat Rock";
     }else if (playerSelection === "scissors" && computerSelection === "scissors"){
-        return "It was a tie. Play again to determine the winner out of five!";
+        return "It was a tie, no points added";
     }else if (playerSelection === "scissors" && computerSelection === "rock"){
+        playerLosses += 1;
         return "You lose! Scissors lost to Rock";
     }else if (playerSelection === "scissors" && computerSelection === "paper"){
+        playerWins += 1;
         return "You won! Scissors beat Paper";
     }
 }
-
-
-
-/*function game(){
-    let playerWins = 0;
-    let computerWins = 0;
-    let playerChoice = '';
-
-    for(let i = 0; i < 5; i++){
-        do{
-            playerChoice = prompt('Please select "Rock", "Paper", or "Scissors"');
-            playerChoice = playerChoice.toLowerCase();
-        }while(playerChoice !== "rock" && playerChoice != "paper" && playerChoice != "scissors");
-        
-        result = playRound(playerChoice, getComputerChoice());
-        alert(result);
-
-        if (result.slice(0, 7) === "You won"){
-            playerWins += 1;
-        } else if (result.slice(0, 8) === "You lose"){
-            computerWins += 1;
-        } else{
-            i -= 1;
-        }
-    }
-    if (playerWins > computerWins){
-        alert(`You won the majority of matches against the computer! You won ${playerWins} to ${computerWins}`);
-    }else if (computerWins > playerWins){
-        alert(`The computer won the majority of matches. You lost ${playerWins} to ${computerWins}!`);
-    }
-}*/
-
